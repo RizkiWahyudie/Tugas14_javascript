3ad7a5c27c4974ecee806dea379a15359ca833b8
rizki wahyudie git <rizkiwahyudie@gmail.com>
2022-02-05 16:42:17 +0700

    add file

---
+++ tugas14.js
@@ -0,0 +1,8 @@
+var x = 37/10;
+console.log(x)
+// console.log("Ceil: Pembulatan keatas")
+console.log(Math.ceil(x));
+// console.log("Floor: Pembulatan kebawah")
+console.log(Math.floor(x));
+// console.log("Round: Pembulatan sesuai belakang koma:")
+console.log(Math.round(x));
